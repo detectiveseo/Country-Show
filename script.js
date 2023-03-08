a3d865f72aa75384b245ec45145f94bd27118c92
detectiveseo <detectiveseo1@gmail.com>
2023-03-08 21:10:56 +0600

    showing country after serch

--- script.js
+++ script.js
@@ -2,11 +2,30 @@ const loadData = async(searchValue) => {
     const url = `https://restcountries.com/v3.1/name/${searchValue}`;
     const res = await fetch(url);
     const dataJson = await res.json();
-    console.log(dataJson)
+    showData(dataJson)
 }
 
 const searchFilter = () => {
     const searchFiled = document.getElementById('search_filed');
     const searchValue = searchFiled.value;
     loadData(searchValue);
+    document.getElementById('search_filed').value = "";
+}
+
+const showData = (datas) => {
+    const countryWarper = document.getElementById('countrys_warper');
+    const {name, capital, continents, flags, currencies} = datas[0];
+    const result = document.createElement('div');
+    result.classList.add('grid');
+    console.log(datas[0])
+    result.innerHTML = `
+        <img src=${flags.png} alt="flug">
+        <h3 class="text-4xl font-semibold">${name.official}</h3>
+        <div class="text-left">
+            <p>${continents}</p>
+            <p>${Object.keys(currencies)}</p>
+        </div>
+    `
+    document.getElementById('countrys_warper').innerText = ' ';
+    countryWarper.appendChild(result)
 }
