f741b1bb365bd0343228d6580069a09eab887014
detectiveseo <detectiveseo1@gmail.com>
2023-03-08 20:37:21 +0600

    connect with API and get the data

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello Emon")
+const loadData = async(searchValue) => {
+    const url = `https://restcountries.com/v3.1/name/${searchValue}`;
+    const res = await fetch(url);
+    const dataJson = await res.json();
+    console.log(dataJson)
+}
+
+const searchFilter = () => {
+    const searchFiled = document.getElementById('search_filed');
+    const searchValue = searchFiled.value;
+    loadData(searchValue);
+}
